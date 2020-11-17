c406d13a75fa71b8c1fe94b0916ec4b18ec0c0d2
logeybogey <michaeldcjohnson@gmail.com>
2020-11-17 20:56:51 +0600

    displaying category input

--- src/category.js
+++ src/category.js
@@ -1,8 +1,35 @@
+let categoryCollection = [];
+let counter = 0
+
+
 function Category(name, tasks){
-    const deleteCategory = () => {
-        console.log(`Deleted ${name}`)
+    
+    const addName = () => {
+        let categoryInputTable = document.querySelector('.categoryInputTableActive')
+        categoryCollection.push(name)
+        console.log(categoryCollection)
+        categoryInputTable.classList.remove('categoryInputTableActive')
+        categoryInputTable.classList.add('categoryInputTable')
     }
-    return{name, tasks, deleteCategory}
+
+    const displayCategory = () => {
+        categoryCollection.forEach(category => {
+            bottomLeftCategoryContainer.textContent = ''
+            let newCategoryContainer = document.createElement('p')
+            newCategoryContainer.classList.add('newCategory')
+            newCategoryContainer.textContent = category
+            bottomLeftCategoryContainer.appendChild(newCategoryContainer)
+            newCategoryContainer.setAttribute('id', `${counter}`)
+            newCategoryContainer.setAttribute('data-index', `${counter}`)
+
+            let deleteCategoryIcon = document.createElement('i')
+            deleteCategoryIcon.setAttribute('class', 'fa fa-trash deleteCategoryIcon')
+            deleteCategoryIcon.setAttribute('data-index', `${counter}`)
+            newCategoryContainer.appendChild(deleteCategoryIcon)
+            counter ++
+        })
+    }
+    return{name, tasks, addName, displayCategory}
 }
 
 export {Category}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import {Category} from './category.js'
 
-const firstCategory = Category ('work', 'finish Marcus')
+const displayCategoryInput = document.getElementById('addCategoryButton')
+const submitCategory = document.getElementById('submitCategory')
+let categoryInputTable = document.querySelector('.categoryInputTable')
 
-firstCategory.deleteCategory()
+
+displayCategoryInput.addEventListener('click', () => {
+    categoryInputTable.classList.remove('categoryInputTable')
+    categoryInputTable.classList.add('categoryInputTableActive')
+})
+
+submitCategory.addEventListener('click', function(){
+    const categoryName = document.getElementById('categoryInputField').value
+    const newCategory = Category(categoryName, 0)
+    newCategory.addName()
+    newCategory.displayCategory()
+})
